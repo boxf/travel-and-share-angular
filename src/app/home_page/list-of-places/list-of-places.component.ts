fddcdb2b5c2fb5d002f57120af5e0d994a6cdd0d
boxf <61790360+boxf@users.noreply.github.com>
2020-03-17 10:58:10 +0100

    Creation of the major components and packages

---
+++ src/app/home_page/list-of-places/list-of-places.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-of-places',
+  templateUrl: './list-of-places.component.html',
+  styleUrls: ['./list-of-places.component.css']
+})
+export class ListOfPlacesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/home_page/place-summary/place-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-place-summary',
+  templateUrl: './place-summary.component.html',
+  styleUrls: ['./place-summary.component.css']
+})
+export class PlaceSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
