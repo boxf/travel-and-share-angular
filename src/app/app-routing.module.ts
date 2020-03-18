17d3cf6c08385f4dfc753177ac5f1925d2d57815
MarionPradeau <MarionPradeau@users.github.com>
2020-03-18 17:22:54 +0100

    Resolve the ivy problem for router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {PlaceSummaryComponent} from './home_page/place-summary/place-summary.component';
 import {RouterModule, Routes} from '@angular/router';
 import {PlaceDescriptionComponent} from './detailplace_page/place-description/place-description.component';
 
