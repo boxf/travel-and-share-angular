0162b12e88d40dc2b0a5d9d0e4c1d162e1224f3e
ramiHarmand <harmand1994@outlook.fr>
2020-03-18 09:12:24 +0100

    add class place.ts, add enum TypeEnum

---
+++ src/app/place.ts
@@ -0,0 +1,13 @@
+import {TypeEnum} from './TypeEnum';
+
+export class Place {
+  id: number;
+  name: string;
+  grade: number;
+  pictureName: string;
+  userReview: string;
+  xaxis: number;
+  yaxis: number;
+  typeEnum: TypeEnum;
+  // type and county
+}
